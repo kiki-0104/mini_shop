92df86b69369c0e23aa78888e44f4a4f0aebfcf8
kiki-0104 <107b20263@mailst.cjcu.edu.tw>
2020-10-27 15:12:40 +0800

    first commit

---
+++ templates_c/9d14570e24485f7e027138c7fc20202e26951836_0.file.goods_form.html.php
@@ -0,0 +1,85 @@
+<?php
+/* Smarty version 3.1.29, created on 2020-10-27 06:13:46
+  from "C:\UniServerZ\www\mini_shop\mini_shop\templates\goods_form.html" */
+
+if ($_smarty_tpl->smarty->ext->_validateCompiled->decodeProperties($_smarty_tpl, array (
+  'has_nocache_code' => false,
+  'version' => '3.1.29',
+  'unifunc' => 'content_5f97ba9a73c3b0_14884544',
+  'file_dependency' => 
+  array (
+    '9d14570e24485f7e027138c7fc20202e26951836' => 
+    array (
+      0 => 'C:\\UniServerZ\\www\\mini_shop\\mini_shop\\templates\\goods_form.html',
+      1 => 1603778873,
+      2 => 'file',
+    ),
+  ),
+  'includes' => 
+  array (
+  ),
+),false)) {
+function content_5f97ba9a73c3b0_14884544 ($_smarty_tpl) {
+?>
+<h1>編輯商品</h1>
+<?php echo '<script'; ?>
+ src="class/ckeditor/ckeditor.js"><?php echo '</script'; ?>
+>
+<form action="tool.php" method="post" class="form-horizontal" role="form" enctype="multipart/form-data">
+    <div class="form-group">
+        <label class="col-md-2 control-label">商品名稱：</label>
+        <div class="col-md-10">
+            <input type="text" class="form-control" name="goods_title" id="goods_title" placeholder="請輸入商品名稱" value="<?php echo $_smarty_tpl->tpl_vars['goods']->value['goods_title'];?>
+">
+        </div>
+    </div>
+
+    <div class="form-group">
+        <label class="col-md-2 control-label">商品內容：</label>
+        <div class="col-md-10">
+            <textarea class="form-control" name="goods_content" id="goods_content" placeholder="請輸入商品內容"><?php echo $_smarty_tpl->tpl_vars['goods']->value['goods_content'];?>
+</textarea>
+        </div>
+    </div>
+
+    <div class="form-group">
+        <label class="col-md-2 control-label">商品價格：</label>
+        <div class="col-md-10">
+            <input type="text" class="form-control" name="goods_price" id="goods_price" placeholder="請輸入商品價格" value="<?php echo $_smarty_tpl->tpl_vars['goods']->value['goods_price'];?>
+">
+        </div>
+    </div>
+
+    <div class="form-group">
+        <label class="col-md-2 control-label">商品圖片：</label>
+        <div class="col-md-10">
+            <input type="file" name="goods_pic" id="goods_pic">
+            <?php if (isset($_smarty_tpl->tpl_vars['goods']->value['pic'])) {?>
+                <img src="<?php echo $_smarty_tpl->tpl_vars['goods']->value['pic'];?>
+" alt="<?php echo $_smarty_tpl->tpl_vars['goods']->value['goods_title'];?>
+">
+            <?php }?>
+        </div>
+    </div>
+
+    <div class="form-group">
+        <div class="col-md-offset-2 col-md-10">
+            <?php if (isset($_smarty_tpl->tpl_vars['goods']->value['goods_sn']) && $_smarty_tpl->tpl_vars['goods']->value['goods_sn'] > 0) {?>
+                <input type="hidden" name="op" value="update_goods">
+                <input type="hidden" name="goods_sn" value="<?php echo $_smarty_tpl->tpl_vars['goods']->value['goods_sn'];?>
+">
+            <?php } else { ?>
+                <input type="hidden" name="op" value="insert_goods">
+            <?php }?>
+            <button type="submit" class="btn btn-primary">儲存商品</button>
+        </div>
+    </div>
+</form>
+
+<?php echo '<script'; ?>
+>
+    CKEDITOR.replace( 'goods_content' );
+<?php echo '</script'; ?>
+>
+<?php }
+}
